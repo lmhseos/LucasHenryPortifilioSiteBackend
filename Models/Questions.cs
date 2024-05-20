a5f28476821fe80b9af8ce74f95aea9983c1ecb3
Lucas Henry <lhenry@bizstream.com>
2024-05-20 11:43:48 -0400

    first commit

---
+++ Models/Questions.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace RAGSystemAPI.Models
+{
+    public class Question
+    {
+        public string Text { get; set; }
+        public string Answer { get; set; }
+        public List<string> Sources { get; set; }
+    }
+}
---
+++ Services/RAGService.cs
@@ -0,0 +1,43 @@
+﻿using dotenv.net;
+using Microsoft.KernelMemory;
+using RAGSystemAPI.Models;
+
+namespace RAGSystemAPI.Services
+{
+    public class RAGService
+    {
+        private readonly MemoryServerless _memory;
+
+        public RAGService()
+        {
+            DotEnv.Load();
+            var env = Environment.GetEnvironmentVariable("OPEN_API_KEY");
+
+            _memory = new KernelMemoryBuilder()
+                .WithOpenAIDefaults(env)
+                .Build<MemoryServerless>();
+        }
+
+        public async Task ImportDocumentAsync(string path, string documentId)
+        {
+            await _memory.ImportDocumentAsync(path, documentId);
+        }
+
+        public async Task<bool> IsDocumentReadyAsync(string documentId)
+        {
+            return await _memory.IsDocumentReadyAsync(documentId);
+        }
+
+        public async Task<Question> AskAsync(string input)
+        {
+            var answer = await _memory.AskAsync(input);
+
+            return new Question
+            {
+                Text = input,
+                Answer = answer.Result,
+                Sources = answer.RelevantSources.Select(x => $"{x.SourceName} - {x.Link} [{x.Partitions.First().LastUpdate:D}]").ToList()
+            };
+        }
+    }
+}
