2384140374536894d1278f97ed377ca46625e545
Lucas Henry <lhenry@bizstream.com>
2024-05-21 11:13:18 -0400

    added db

---
+++ PersonalSiteBackend/Controllers/RAGController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RAGSystemAPI.Services;
+using System.Threading.Tasks;
+using PersonalSiteBackend.DTO;
+
+namespace RAGSystemAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RagController(RagService ragService) : ControllerBase
+    {
+        [HttpPost("import")]
+        public async Task<IActionResult> ImportDocument([FromBody] DocumentDto documentDto)
+        {
+            await ragService.ImportDocumentAsync(documentDto);
+
+            if (await ragService.IsDocumentReadyAsync(documentDto.Id))
+            {
+                return Ok(new { Message = "Document imported successfully." });
+            }
+            else
+            {
+                return StatusCode(500, new { Message = "Document import failed." });
+            }
+        }
+
+        [HttpPost("ask")]
+        public async Task<IActionResult> AskQuestion([FromBody] AskQuestionDto questionDto)
+        {
+            var result = await ragService.AskAsync(questionDto.Text);
+            return Ok(result);
+        }
+    }
+}
--- PersonalSiteBackend/obj/Debug/net8.0/PersonalSiteBackend.AssemblyInfo.cs
+++ PersonalSiteBackend/obj/Debug/net8.0/PersonalSiteBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonalSiteBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d06f57952544a16986731fef314edb4ef998fdf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonalSiteBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonalSiteBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
